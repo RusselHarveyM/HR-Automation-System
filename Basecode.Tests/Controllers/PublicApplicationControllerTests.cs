a980c9ca91b2b7fcc057803ea91f2df62a2c155e
Brent Lasaca <brent.lasaca@cit.edu>
2023-07-09 15:47:56 +0800

    Create unit tests for Public Application Controller

--- Basecode.Tests/Controllers/PublicApplicationControllerTests.cs
+++ Basecode.Tests/Controllers/PublicApplicationControllerTests.cs
@@ -1,5 +1,10 @@
 ﻿using Basecode.WebApp.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Basecode.Tests.Controllers
 {
